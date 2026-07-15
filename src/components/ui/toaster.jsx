69c3256ceaa293713c73f76d8e9709b38a9e9835
Denzelsmith Valdez <denzelsmithvaldez@gmail.com>
2026-07-15 01:38:30 +0000

    Visual edits

--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -14,17 +14,17 @@ export function Toaster() {
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props} className="hidden">
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description &&
-              <ToastDescription>{description}</ToastDescription>
-              }
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>);
+        return null;
+
+
+
+
+
+
+
+
+
+
 
       })}
       <ToastViewport />
