81a4107b55fa4653a36f32594d167a85662fe09c
Denzelsmith Valdez <denzelsmithvaldez@gmail.com>
2026-07-16 23:31:26 +0000

    Visual edits

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,14 @@
 export const LOGO_URL =
-  "https://media.base44.com/images/public/6a5676aa61ea8a51237aa4ee/89dfc5da1_IMG_9177.png";
+"https://media.base44.com/images/public/6a5676aa61ea8a51237aa4ee/89dfc5da1_IMG_9177.png";
 
 export default function Logo({ size = 26, className = "" }) {
   return (
     <img
       src={LOGO_URL}
       alt="PUBLIC."
-      className={`logo-dark-invert ${className}`}
+      className={`logo-dark-invert my-1 ${className}`}
       style={{ height: size, width: "auto", display: "block" }}
-      draggable={false}
-    />
-  );
+      draggable={false} />);
+
+
 }
