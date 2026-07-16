c51b949d38fe025f18860d72b7431e9ce20b2b45
Denzelsmith Valdez <denzelsmithvaldez@gmail.com>
2026-07-16 20:21:38 +0000

    Visual edits

--- src/components/FullScreenPlayer.jsx
+++ src/components/FullScreenPlayer.jsx
@@ -51,9 +51,9 @@ export default function FullScreenPlayer({ onClose, onOpenQueue }) {
           <div className="text-[11px] uppercase tracking-[0.25em] opacity-70">
             Now Playing
           </div>
-          <button className="p-2 -mr-2 hidden" aria-label="More">
-            <MoreHorizontal size={24} />
-          </button>
+          
+
+          
         </div>
 
         {/* album art */}
